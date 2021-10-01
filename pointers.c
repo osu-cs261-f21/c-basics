2a4c705295a3584a84143be455113c97edf526a3
Rob Hess <hessro@oregonstate.edu>
2021-10-01 11:30:59 -0700

    Add code from 10/1 lecture.

--- pointers.c
+++ pointers.c
@@ -24,5 +24,21 @@ int main(int argc, char const *argv[]) {
   times_2(&n);
   printf("\n== n: %d\n", n);
 
+  void* v_ptr = &n;
+  printf("\n== v_ptr: %p\n", v_ptr);
+  printf("== (int*)v_ptr: %p\n", (int*)v_ptr);
+  // printf("== *v_ptr: %d\n", *v_ptr);
+  printf("== *(int*)v_ptr: %d\n", *(int*)v_ptr);
+
+  int* n_ptr2 = v_ptr;
+  printf("== *n_ptr2: %d\n", *n_ptr2);
+
+  float pi = 3.1415;
+  v_ptr = &pi;
+  n_ptr2 = v_ptr;
+  printf("\n== v_ptr: %p\n", v_ptr);
+  printf("== &pi: %p\n", &pi);
+  printf("== *n_ptr2: %d\n", *n_ptr2);
+
   return 0;
 }
