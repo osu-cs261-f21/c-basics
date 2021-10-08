be2f76947e6279d382b51bdc92956503fa056fb4
Rob Hess <hessro@oregonstate.edu>
2021-10-08 11:06:57 -0700

    Add code from 10/8 lecture.

--- strings.c
+++ strings.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
 
@@ -8,6 +9,13 @@ int main(int argc, char const *argv[]) {
   string[1] = 'a';
   string[2] = 't';
   string[3] = '\0';
+
+  const char* name = "Luke Skywalker";
+  // name[1] = 'i';
+
+  int n = strlen(name);
+  char* copy = malloc((n + 1) * sizeof(char));
+  strncpy(copy, name, n + 1);
 
   return 0;
 }
